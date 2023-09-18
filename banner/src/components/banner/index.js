6361f554dfd403aa2753f18023a2d218bed4303a
AndyJones1989 <andrew.jones@parcel2go.com>
2023-09-18 09:38:00 +0100

    Banner component added

---
+++ banner/src/components/banner/index.js
@@ -0,0 +1,9 @@
+
+function Banner () {
+
+    return (
+        <div style={{fontSize: '50px', fontWeight: 'bolder', backgroundColor: 'black', color:'white', width: '80%', margin: '40px auto', borderRadius: '8px'}}> Andy's To-Do List </div>
+    )
+}
+
+export default Banner;
