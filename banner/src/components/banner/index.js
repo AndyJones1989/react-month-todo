65531d0047b4ff82a87113d80ea192c35dcfce02
AndyJones1989 <andrew.jones@parcel2go.com>
2023-09-22 14:59:24 +0100

    format banner

--- banner/src/components/banner/index.js
+++ banner/src/components/banner/index.js
@@ -1,9 +1,19 @@
-
-function Banner () {
-
-    return (
-        <div style={{fontSize: '50px', fontWeight: 'bolder', backgroundColor: 'black', color:'white', width: '80%', margin: '40px auto', borderRadius: '8px'}}> Andy's To-Do List </div>
-    )
+function Banner() {
+  return (
+    <div
+      style={{
+        fontSize: "50px",
+        fontWeight: "bolder",
+        backgroundColor: "black",
+        color: "white",
+        width: "80%",
+        margin: "40px auto",
+        borderRadius: "8px",
+      }}
+    >
+      Andy's To-Do List
+    </div>
+  );
 }
 
 export default Banner;
